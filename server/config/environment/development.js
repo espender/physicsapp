ceb51ed4dbf1f3f4a559ccad5e07f4ebba11f197
espender <elliottspender@googlemail.com>
2015-04-16 16:57:22 +0100

    Begin physics app project

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/physicsapp-dev'
+  },
+
+  seedDB: true
+};
